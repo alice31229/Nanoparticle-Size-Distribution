e4117635f1ea87d43a10ec2abf17c6a948ea5de4
alice31229 <51815935+alice31229@users.noreply.github.com>
2021-03-09 15:29:22 +0800

    Update SVM_HOG_nanoparticle.py

--- SVM_HOG_nanoparticle.py
+++ SVM_HOG_nanoparticle.py
@@ -1,15 +1,3 @@
-# https://github.com/SamPlvs/Object-detection-via-HOG-SVM  # code source
-
-# add threshold or adjust resolution
-
-
-
-### (1)run every img in a file -> (2)crop detected nanoparticles and use RANSAC to measure size -> (3)plot distribution ###
-
-
-### detail to rewrite:  save detection result; save detected particle size 
-
-
 import pandas as pd
 from skimage import feature, measure, draw
 from skimage.feature import hog
@@ -28,10 +16,7 @@ import cv2
 import os
 
 
-########################### (1) ############################
-
-#Define HOG Parameters
-# change them if necessary to orientations = 8, pixels per cell = (16,16), cells per block to (1,1) for weaker HOG
+# define HOG Parameters
 orientations = 13
 pixels_per_cell = (8, 8)
 cells_per_block = (2, 2)
@@ -46,27 +31,19 @@ def sliding_window(image, stepSize, windowSize):# image is the input, step size 
         for x in range(0, image.shape[1], stepSize):
             # yield the current window
             yield (x, y, image[y: y + windowSize[1], x: x + windowSize[0]])
-#%%
-# Upload the saved svm model:
-#model = joblib.load('model_new32.npy')   # HT crop 1 t.d.
 
 def File_imgs_overall(file,dir):
 	# load df for diameter
     df = pd.read_excel(dir+'/'+file, engine='openpyxl')
     
     d_R = df['Diameter(nm)']
-    
-    #https://stackoverflow.com/questions/37374983/get-data-points-from-seaborn-distplot/37375147
-    #hist = plt.hist(d_R, edgecolor='black', alpha = 0.25, bins=10)
+   
     ax = sns.distplot(d_R, hist = False, kde = True,
                 kde_kws = {'linewidth': 1})
 
     d_avg.append(np.mean(d_R))
     d_std.append(np.std(d_R))
-    #print(ax.patches)
     
-    # https://kknews.cc/zh-tw/news/mq2bn3z.html
-    #原文網址：https://kknews.cc/news/mq2bn3z.html
     
     plt.title('Nanoparticle size distribution')
     plt.xlabel('Particle diameter(nm)')
@@ -74,7 +51,6 @@ def File_imgs_overall(file,dir):
     plt.xlim(5, 30)
 
 
-###################################################################################
 def detect(file,dir):
     
     # read image in
@@ -103,7 +79,8 @@ def detect(file,dir):
 	windowSize=(winW,winH)
 	downscale=1.15 
 	stepSize = 2
-
+	
+	# upload the pretrained SVM 
 	model = joblib.load('model_new2.npy')
 
 	for resized in pyramid_gaussian(img, max_layer=-1, downscale=1.15): # loop over each layer of the image that you take! 5, -1
@@ -148,22 +125,12 @@ def detect(file,dir):
 	    RD.append((xB, yB))
 
 	cv2.imwrite(os.path.join(r'./detection_result', 'ML23x0.4s2_{}'.format(file), img1)) 
-	# os.path.join(r'./detection_result', 'ML23x0.4s2_{}'.format(file), img1)
 
-	
-	# save excel file for 'Diameter(nm)' and 'Center coordinate'
-	#df = pd.DataFrame(list(zip(LU, RD)), 
-	#                  columns =['Left up coordinate', 'Right down coordinate'])
-	
-
-	########################### (2) ############################	
+		
 
 	diameter_pixel_o = []
 
-	#img = cv2.imread('20200904_NYF_R/'+'{}.png'.format(file))
 	img = cv2.imread(dir+'/'+file, 0)
-	#img = cv2.imread("{}.png".format(file))
-	#img = cv2.imread("{}.jpg".format(file))
 	scale_percent = 40 # percent of original size 100 in scale bar   80
 	width = int(img.shape[1] * scale_percent / 100)
 	height = int(img.shape[0] * scale_percent / 100)
@@ -173,44 +140,39 @@ def detect(file,dir):
     
     
 	for i in range(len(LU)):
-	# 裁切區域的 center: x 與 y 座標
+		# center of crop img: x and y coordinate
 		x_1 = LU[i][0]
 		y_1 = LU[i][1]
 		x_2 = RD[i][0]
 		y_2 = RD[i][1]
-	# 裁切圖片  
+		# crop img  
 		crop_img = img[y_1:y_2, x_1:x_2]
 
 		shape = crop_img.shape
 
 		# RANSAC
 
-        #closing
+        	#closing
 		kernel = np.ones((11,11), np.uint8)
 		img2 = cv2.morphologyEx(crop_img, cv2.MORPH_CLOSE, kernel)
 
-        #sharpening
+        	#sharpening
 		kernel = np.array([[-1,-1,-1], [-1,9,-1], [-1,-1,-1]])
 		img2 = cv2.filter2D(img2, -1, kernel)
 
-        #test if you use different scale img than 0.2 of the original that I used
-        #remember that the actual kernel size for GaussianBlur is trackbar_pos3*2+1
-        #you want to get as full circle as possible here
-        #canny = CannyTrackbar(img, "canny_trakbar")
-
-        #additional blurring to reduce the offset toward brighter region
+        	#additional blurring to reduce the offset toward brighter region
 		img_blurred = cv2.GaussianBlur(img2.copy(), (8*2+1,8*2+1), 1)
 
-        ### try try ###
+		#equalizing
 		equ = cv2.equalizeHist(img_blurred)
 		mean_val = round(equ[:,:].mean()*(2/3))
 
-        # binary 
+        	#binary 
 		ret, th = cv2.threshold(img_blurred, mean_val, 250, cv2.THRESH_BINARY) 
 
 		cannyMax = ret 
 
-		#detect edge. important: make sure this works well with CannyTrackbar()
+		#detect edge
 		canny = cv2.Canny(img_blurred, mean_val*0.66, mean_val*1.33)
 
 		coords = np.column_stack(np.nonzero(canny))
@@ -220,12 +182,13 @@ def detect(file,dir):
 		                                max_trials=150)
 		                                
 
-		if int(model.params[0]) + model.params[2] <= shape[0] and int(model.params[0]) - model.params[2] > 0 and int(model.params[1]) + model.params[2] <= shape[0] and int(model.params[1]) - model.params[2] > 0:
+		if int(model.params[0]) + model.params[2] <= shape[0] and int(model.params[0]) - model.params[2] > 0 and int(model.params[1]) + model.params[2] <= shape[0] \
+		and int(model.params[1]) - model.params[2] > 0:
 
 		    diameter_pixel_o.append(2*model.params[2]*2.5*scale_bar)
-		    #diameter_nm.append(2*model.params[2]*2.5*scale_bar) 
+		    
 
-	# remove outliers
+	# remove outliers (some false positive detection results)
 	z = np.abs(stats.zscore(diameter_pixel_o))
 
 	diameter_pixel = np.array(diameter_pixel_o)[z < 3]
@@ -234,9 +197,8 @@ def detect(file,dir):
 	df = pd.DataFrame(diameter_pixel, columns =['Diameter(nm)'])
 	df.to_excel(os.path.join(r'./diameter(nm)', '{}.xlsx'.format(file)))
 
-    ########################### (3) ############################
-    # plot distribution 
-	# Draw diameter list histogram
+    
+    	# plot distribution 
 	hist = plt.hist(diameter_pixel, edgecolor='black', bins=10, color='green')
 
 	plt.title('Nanoparticle size distribution')
@@ -274,7 +236,6 @@ for file in os.listdir(directory):
 
 df_all = pd.DataFrame(diameter_nm, columns =['Diameter(nm)'])
 df_all.to_excel(os.path.join(r'./', 'Overall.xlsx'))
-###################################################################################
 
 directory_2 = r'./diameter(nm)'
 d_avg = []
@@ -295,7 +256,7 @@ diameter_nm = dff['Diameter(nm)']
 n = len([name for name in os.listdir(directory_2) if os.path.isfile(os.path.join(directory_2, name))]) 
 main = sns.distplot(diameter_nm, hist = False, kde = True, label="distribution of all {} images".format(str(n)),
                     kde_kws = {'linewidth': 3, "color": "k"})
-#d_r.set_label('distribution of all 29 images')
+
 
 lg = plt.legend(labels=['distribution of all {} images'.format(str(n))], bbox_to_anchor=(1.05, 1.0), loc='upper left')
 
@@ -306,7 +267,7 @@ plt.savefig(os.path.join(r'./', "hist_overlap_addmain"),
 
 fig, ax = plt.subplots()
 
-true = ax.axhline(y=np.mean(diameter_nm), xmin=0.1, xmax=0.9, ls='--') #畫線
+true = ax.axhline(y=np.mean(diameter_nm), xmin=0.1, xmax=0.9, ls='--') 
 
 d = ax.scatter(range(len(d_avg)), d_avg, alpha = 0.25, s=30, c='green') # avg of each image
 d_1 = ax.scatter(range(len(d_avg)), np.array(d_avg)-np.array(d_std), alpha = 0.25, s=30, c='red')
@@ -318,10 +279,9 @@ for i in range(len(d_avg)):
 
 lg2 = plt.legend([d, d_1, true], ['mean of each image', 'mean±standard deviation of each image', "mean of all images"], bbox_to_anchor=(1.05, 1.0), loc='upper left')
 
-plt.xlabel("Each {} image".format(str(n)), fontsize=13) #x軸標題
-plt.ylabel("Mean±std diameter(nm)", fontsize=13) #y軸標題
-#plt.legend(handles=[dr, db, dm], loc='upper left')
-plt.grid(alpha=0.75) #虛線 對齊
+plt.xlabel("Each {} image".format(str(n)), fontsize=13) 
+plt.ylabel("Mean±std diameter(nm)", fontsize=13) 
+plt.grid(alpha=0.75) 
 plt.savefig(os.path.join(r'./', "diameter_mean"), 
             bbox_extra_artists=(lg2,), 
             bbox_inches='tight')
